28e1bab1dcd90ab1a7cb3ab95989296e8df9a22d
Daniel Carrillo <carrdanillo@gmail.com>
2019-06-29 00:03:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } else if (string.toUpperCase() === string) {
     return"YES INDEED!"
-  } else if (string === "I love")
+  } else if (string === "I love you, Gradma.") {
+    return "I love you, too."
+  }
 }
 
 
